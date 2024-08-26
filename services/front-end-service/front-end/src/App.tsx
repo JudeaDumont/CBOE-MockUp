27cd794d83e0ea7f45c4f725139bebaeb7c3ed33
Judea Dumont <dumontjudea2@gmail.com>
2024-08-26 11:00:02 -0700

    could not get test containers working,
    next i am going to setup the UI to where I can subscribe to a kafka stream and watch real time data

--- services/front-end-service/front-end/src/App.tsx
+++ services/front-end-service/front-end/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Login from './components/login';
 import Profile from './components/profile';
-import CreateUser from "./components/createUser.tsx"; // This will be the new component for the landing page
+import CreateUser from "./components/createUser.tsx";
+import Landing from "./components/landing.tsx"; // This will be the new component for the landing page
 
 const App: React.FC = () => {
     const token = localStorage.getItem('jwtToken');
@@ -13,8 +14,8 @@ const App: React.FC = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/create-user" element={<CreateUser />} />
-                <Route path="/profile" element={token ? <Profile /> : <Navigate to="/" />} />
-                <Route path="/" element={token ? <Profile /> : <Profile />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/" element={token ? <Profile /> : <Landing />} />
             </Routes>
         </Router>
     );
