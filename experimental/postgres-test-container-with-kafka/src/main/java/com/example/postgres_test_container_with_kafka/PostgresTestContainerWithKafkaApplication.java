3fbb44ae09fd9205e7f43f969508553bd5199ae3
Judea Dumont <dumontjudea2@gmail.com>
2024-09-04 17:50:47 -0700

    initial commit for a postgres with testcontainers service

---
+++ experimental/postgres-test-container-with-kafka/src/main/java/com/example/postgres_test_container_with_kafka/PostgresTestContainerWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgres_test_container_with_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresTestContainerWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresTestContainerWithKafkaApplication.class, args);
+	}
+
+}
