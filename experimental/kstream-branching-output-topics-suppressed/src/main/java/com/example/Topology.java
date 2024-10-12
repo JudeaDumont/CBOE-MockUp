d22a254ec705f0459842aa3abd7f0469f2825f20
dumont, judea <dumontjudea2@gmail.com>
2024-10-12 15:39:06 -0700

    finished examples of both suppressed and non suppressed examples of branching output topics.
    the single output topic example will emit intermdeiate results if not suppressed. and the branching output topic will only emit results if you send a second batch of messages with an advanced timestamp.

---
+++ experimental/kstream-branching-output-topics-suppressed/src/main/java/com/example/Topology.java
@@ -0,0 +1,69 @@
+
+package com.example;
+
+import java.time.Duration;
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
+public class Topology {
+	private final String inputTopic;
+
+	private final String outputTopicA;
+	private final String outputTopicB;
+	private final String outputTopicC;
+
+	@Autowired
+	public Topology(@Value("${input-topic.name}") final String inputTopic,
+					@Value("${output-topic-a.name}") final String outputTopicA,
+					@Value("${output-topic-b.name}") final String outputTopicB,
+					@Value("${output-topic-c.name}") final String outputTopicC) {
+		this.inputTopic = inputTopic;
+		this.outputTopicA = outputTopicA;
+		this.outputTopicB = outputTopicB;
+		this.outputTopicC = outputTopicC;
+	}
+
+	@Autowired
+	public void defaultTopology(final StreamsBuilder builder) {
+		KStream<String, String> stream = builder.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()));
+		Map<String, KStream<String, String>> branches =
+				stream.split(Named.as("Branch-"))
+						.branch((key, value) -> key.startsWith("A"),
+								Branched.as("A"))
+						.branch((key, value) -> key.startsWith("B"),
+								Branched.as("B"))
+						.defaultBranch(Branched.as("C"));
+
+		branches.get("Branch-A")
+				.groupByKey()
+				.count()
+				.suppress(Suppressed.untilTimeLimit(Duration.ofMillis(5), Suppressed.BufferConfig.unbounded()))
+				.toStream()
+				.to(outputTopicA);
+
+		branches.get("Branch-B")
+				.groupByKey()
+				.count()
+				.suppress(Suppressed.untilTimeLimit(Duration.ofMillis(5), Suppressed.BufferConfig.unbounded()))
+				.toStream()
+				.to(outputTopicB);
+
+		branches.get("Branch-C")
+				.groupByKey()
+				.count()
+				.suppress(Suppressed.untilTimeLimit(Duration.ofMillis(5), Suppressed.BufferConfig.unbounded()))
+				.toStream()
+				.to(outputTopicC);
+	}
+}
---
+++ experimental/kstream-branching-output-topics-suppressed/src/test/java/com/example/BranchingTopicsSuppressedTests.java
@@ -0,0 +1,88 @@
+
+package com.example;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+
+@SpringBootTest
+public class BranchingTopicsSuppressedTests {
+	private final Logger logger = LoggerFactory.getLogger(BranchingTopicsSuppressedTests.class);
+
+	private TopologyTestDriver testDriver;
+
+	@Value("${input-topic.name}")
+	private String inputTopicName;
+
+	@Value("${output-topic-a.name}")
+	private String outputTopicNameA;
+
+	@Value("${output-topic-b.name}")
+	private String outputTopicNameB;
+
+	@Value("${output-topic-c.name}")
+	private String outputTopicNameC;
+
+	private TestInputTopic<String,String> inputTopic;
+
+	private TestOutputTopic<String, Long> outputTopicA;
+	private TestOutputTopic<String, Long> outputTopicB;
+	private TestOutputTopic<String, Long> outputTopicC;
+
+	@Autowired
+	private StreamsBuilderFactoryBean streamsBuilder;
+
+	@BeforeEach
+	public void setup() {
+		this.testDriver = new TopologyTestDriver(streamsBuilder.getTopology(), streamsBuilder.getStreamsConfiguration());
+		logger.info(streamsBuilder.getTopology().describe().toString());
+		this.inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.String().serializer(), Serdes.String().serializer());
+		this.outputTopicA = testDriver.createOutputTopic(outputTopicNameA, Serdes.String().deserializer(), Serdes.Long().deserializer());
+		this.outputTopicB = testDriver.createOutputTopic(outputTopicNameB, Serdes.String().deserializer(), Serdes.Long().deserializer());
+		this.outputTopicC = testDriver.createOutputTopic(outputTopicNameC, Serdes.String().deserializer(), Serdes.Long().deserializer());
+	}
+
+	@AfterEach
+	public void after() {
+		if (testDriver != null) {
+			this.testDriver.close();
+		}
+	}
+
+	@Test
+	public void testTopologyLogic() {
+
+		inputTopic.pipeInput("C-msg", "test-C", 1L);
+		inputTopic.pipeInput("B-msg", "test-B", 10L);
+		inputTopic.pipeInput("A-msg", "test-A", 20L);
+
+		inputTopic.pipeInput("C-msg", "test-C", 31L);
+		inputTopic.pipeInput("B-msg", "test-B", 32L);
+		inputTopic.pipeInput("A-msg", "test-A", 33L);
+
+		Awaitility.waitAtMost(Duration.ofSeconds(5)).until(() -> outputTopicA.getQueueSize() == 1L);
+		Awaitility.waitAtMost(Duration.ofSeconds(5)).until(() -> outputTopicB.getQueueSize() == 1L);
+		Awaitility.waitAtMost(Duration.ofSeconds(5)).until(() -> outputTopicC.getQueueSize() == 1L);
+		assertThat(outputTopicA.readValuesToList()).isEqualTo(List.of(2L));
+		assertThat(outputTopicB.readValuesToList()).isEqualTo(List.of(2L));
+		assertThat(outputTopicC.readValuesToList()).isEqualTo(List.of(2L));
+	}
+
+}
