2917cfd231afd4c29850242cadda7745f26626fe
dumont, judea <dumontjudea2@gmail.com>
2024-10-13 18:31:09 -0700

    Flat map duplicating messages example with explicit deserializer due to flat maps ability to change the key and val type

---
+++ experimental/kstream-flat-map-tri-split/src/main/java/com/example/Topology.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Suppressed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
+public class Topology {
+	private final String inputTopic;
+
+	private final String outputTopic;
+
+	@Autowired
+	public Topology(@Value("${input-topic.name}") final String inputTopic,
+					@Value("${output-topic.name}") final String outputTopic) {
+		this.inputTopic = inputTopic;
+		this.outputTopic = outputTopic;
+	}
+
+	@Autowired
+	public void defaultTopology(final StreamsBuilder builder) {
+		builder.stream(inputTopic, Consumed.with(Serdes.Integer(), Serdes.String()))
+				.flatMap(
+						(key, value) -> {
+							List<KeyValue<Integer, String>> result = new LinkedList<>();
+							result.add(KeyValue.pair(key, value + " test-a"));
+							result.add(KeyValue.pair(key, value + " test-b"));
+							return result;
+						}
+				)
+				.to(outputTopic);
+	}
+}
