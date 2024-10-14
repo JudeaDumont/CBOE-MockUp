63771a6f3aea3577cc113c240c1dd5c846e1d1d3
dumont, judea <dumontjudea2@gmail.com>
2024-10-14 12:46:27 -0700

    Removed the readme docs since I do not really make any use of them, and instead I make use of a devlog kept in google docs that I may add later, though it includes much more overarching ideas liek infrastructure using terraform and GCP.
    Included is an example of using flatMapValues to branch 0 one or more records per record into a single topic without repartitioning data, which is a big deal for debuggability, maintainability, and performance, wince that may mean needing to create more partitions etc. Instead, since flatmapvalues only handles the values, the key comes in the same, and only more messages with different values are created and returned for further pipeline processing.

---
+++ experimental/kstream-flat-map-value-only-tri-split/src/test/java/com/example/OutputTopicTest.java
@@ -0,0 +1,78 @@
+
+package com.example;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+
+@SpringBootTest
+public class OutputTopicTest {
+	private final Logger logger = LoggerFactory.getLogger(OutputTopicTest.class);
+
+	private TopologyTestDriver testDriver;
+
+	@Value("${input-topic.name}")
+	private String inputTopicName;
+
+	@Value("${output-topic.name}")
+	private String outputTopicName;
+
+	private TestInputTopic<Integer,String> inputTopic;
+
+	private TestOutputTopic<Integer, String> outputTopic;
+
+	@Autowired
+	private StreamsBuilderFactoryBean streamsBuilder;
+
+	@BeforeEach
+	public void setup() {
+
+		Properties props = streamsBuilder.getStreamsConfiguration();
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+		this.testDriver = new TopologyTestDriver(streamsBuilder.getTopology(), props);
+		logger.info(streamsBuilder.getTopology().describe().toString());
+		this.inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.Integer().serializer(), Serdes.String().serializer());
+		this.outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.Integer().deserializer(), Serdes.String().deserializer());
+	}
+
+	@AfterEach
+	public void after() {
+		if (testDriver != null) {
+			this.testDriver.close();
+		}
+	}
+
+	@Test
+	public void testTopologyLogic() {
+		inputTopic.pipeInput(1, "test1", 1L);
+
+		Awaitility.waitAtMost(Duration.ofSeconds(5)).until(() -> outputTopic.getQueueSize() == 3L);
+
+		assertThat(outputTopic.readValuesToList()).isEqualTo(List.of("test1 test-a", "test1 test-b", "test1 test-c"));
+		assertThat(outputTopic.readKeyValuesToList().get(0).key).isEqualTo(1);
+		assertThat(outputTopic.readKeyValuesToList().get(1).key).isEqualTo(1);
+		assertThat(outputTopic.readKeyValuesToList().get(2).key).isEqualTo(1);
+	}
+}
