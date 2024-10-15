03708ae2472370b3e47e7d82359ccbb6d41f6c10
dumont, judea <dumontjudea2@gmail.com>
2024-10-14 21:09:05 -0700

    Group By Example that changes the type of the key and aggregates the value of two messages together

---
+++ experimental/kstream-grouped-by-changes-key-output-topic/src/main/java/com/example/Topology.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
+public class Topology {
+    private final String inputTopic;
+
+    private final String outputTopic;
+
+    @Autowired
+    public Topology(@Value("${input-topic.name}") final String inputTopic,
+                    @Value("${output-topic.name}") final String outputTopic) {
+        this.inputTopic = inputTopic;
+        this.outputTopic = outputTopic;
+    }
+
+    @Autowired
+    public void defaultTopology(final StreamsBuilder builder) {
+        builder.stream(inputTopic, Consumed.with(Serdes.Integer(), Serdes.String()))
+                .groupBy(
+                        (key, value) -> key.toString(),
+                        Grouped.with(
+                                Serdes.String(),
+                                Serdes.String())
+                )
+                .aggregate(()-> "", (key, value, aggVal)->
+                    aggVal+=value
+                )
+                .toStream()
+                .to(outputTopic);
+    }
+}
