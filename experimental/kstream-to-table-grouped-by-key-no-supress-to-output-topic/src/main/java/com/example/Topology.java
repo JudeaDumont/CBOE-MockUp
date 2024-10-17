fb3e0f8acce9250d3162468c8ac4875490f17fa6
dumont, judea <dumontjudea2@gmail.com>
2024-10-16 19:00:01 -0700

    toTable example which replaces the count that I used to get a table out a stream so I could run a group by that changed the key and value of a ktable. Now it does not change the value but does still replaces updated values in the aggregate with *.

---
+++ experimental/kstream-to-table-grouped-by-key-no-supress-to-output-topic/src/main/java/com/example/Topology.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
+public class Topology {
+    private final String inputTopic;
+
+    private final String outputTopic;
+
+    @Autowired
+    public Topology(@Value("${input-topic.name}") final String inputTopic,
+                    @Value("${output-topic.name}") final String outputTopic) {
+        this.inputTopic = inputTopic;
+        this.outputTopic = outputTopic;
+    }
+
+    @Autowired
+    public void defaultTopology(final StreamsBuilder builder) {
+        builder.stream(inputTopic, Consumed.with(Serdes.Integer(), Serdes.String()))
+                .toTable()
+                .groupBy((key, value) -> KeyValue.pair(key.toString(), value),
+                        Grouped.with(
+                                Serdes.String(),
+                                Serdes.String()))
+                .aggregate(
+                        () -> "",
+                        (aggKey, newValue, aggValue) -> aggValue += newValue,
+                        (aggKey, oldValue, aggValue) -> aggValue.replace(oldValue,"*"))
+                .toStream()
+                .to(outputTopic);
+    }
+}
