74c6ded033872838b7b70f7fcf9307780a2c8e1f
Judea Dumont <dumontjudea2@gmail.com>
2024-10-19 00:49:35 -0700

    Functioning tumbling windowed operation

---
+++ experimental/kstream-tumbling-aggregate/src/main/java/com/example/Topology.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+
+@Configuration
+@Component
+public class Topology {
+    private final String inputTopic;
+
+    private final String outputTopic;
+
+    @Autowired
+    public Topology(@Value("${input-topic.name}") final String inputTopic,
+                    @Value("${output-topic.name}") final String outputTopic) {
+        this.inputTopic = inputTopic;
+        this.outputTopic = outputTopic;
+    }
+
+    @Autowired
+    public void defaultTopology(final StreamsBuilder builder) {
+        builder.stream(inputTopic, Consumed.with(Serdes.Integer(), Serdes.String()))
+                .groupByKey()
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(5)))
+                .aggregate(
+                        () -> "", /* initializer */
+                        (aggKey, newValue, aggValue) -> aggValue += newValue,
+                        Materialized.as("tumbling-state")
+                )
+                .toStream()
+                .map((key, value) -> KeyValue.pair(key.key(), value))
+                .to(outputTopic);
+    }
+}
