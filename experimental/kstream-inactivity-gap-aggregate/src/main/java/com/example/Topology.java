22814fa17c6f28f314a0aa9738c61002eb8af0dc
dumont, judea <dumontjudea2@gmail.com>
2024-10-19 21:43:28 -0700

    inactivity gap example exemplified. So I have the tests for different windows, but am really missing the understanding of why the test results are different.

---
+++ experimental/kstream-inactivity-gap-aggregate/src/main/java/com/example/Topology.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+
+@Configuration
+@Component
+public class Topology {
+    private final String inputTopic;
+
+    private final String outputTopic;
+
+    @Autowired
+    public Topology(@Value("${input-topic.name}") final String inputTopic,
+                    @Value("${output-topic.name}") final String outputTopic) {
+        this.inputTopic = inputTopic;
+        this.outputTopic = outputTopic;
+    }
+
+    @Autowired
+    public void defaultTopology(final StreamsBuilder builder) {
+        builder.stream(inputTopic, Consumed.with(Serdes.Integer(), Serdes.String()))
+                .groupByKey()
+                .windowedBy(SessionWindows.ofInactivityGapWithNoGrace(Duration.ofSeconds(4)))
+                .aggregate(
+                        () -> "",
+                        (aggKey, newValue, aggValue) -> aggValue += newValue,
+                        (aggKey, leftAggValue, rightAggValue) -> leftAggValue += rightAggValue,
+                        Materialized.as("tumbling-state")
+                )
+                .toStream()
+                .map((key, value) -> KeyValue.pair(key.key(), value))
+                .to(outputTopic);
+    }
+}
