d60c1621a8d0ba5ca98cbba7d45ff1b07336a56d
dumont, judea <dumontjudea2@gmail.com>
2024-10-20 00:01:17 -0700

    3 examples of sliding, inactivity-gap, and tumbling counts

---
+++ experimental/kstream-inactivity-gap-count/src/test/java/com/example/OutputTopicTest.java
@@ -0,0 +1,87 @@
+
+package com.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+
+import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class OutputTopicTest {
+	private final Logger logger = LoggerFactory.getLogger(OutputTopicTest.class);
+
+	private TopologyTestDriver testDriver;
+
+	@Value("${input-topic.name}")
+	private String inputTopicName;
+
+	@Value("${output-topic.name}")
+	private String outputTopicName;
+
+	private TestInputTopic<Integer,String> inputTopic;
+
+	private TestOutputTopic<Integer, Long> outputTopic;
+
+	@Autowired
+	private StreamsBuilderFactoryBean streamsBuilder;
+
+	@BeforeEach
+	public void setup() {
+
+		Properties props = streamsBuilder.getStreamsConfiguration();
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
+
+		this.testDriver = new TopologyTestDriver(streamsBuilder.getTopology(), props);
+		logger.info(streamsBuilder.getTopology().describe().toString());
+		this.inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.Integer().serializer(), Serdes.String().serializer());
+		this.outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.Integer().deserializer(), Serdes.Long().deserializer());
+	}
+
+	@AfterEach
+	public void after() {
+		if (testDriver != null) {
+			this.testDriver.close();
+		}
+	}
+
+	@Test
+	public void testTopologyLogic() {
+		inputTopic.pipeInput(1, "test1", 1L);
+		inputTopic.pipeInput(1, "test2", 11L);
+		inputTopic.pipeInput(2, "test3", 21L);
+		inputTopic.pipeInput(2, "test4", 31L);
+		inputTopic.pipeInput(3, "test5", 41L);
+
+		Awaitility.waitAtMost(Duration.ofSeconds(5)).until(() -> outputTopic.getQueueSize() == 7L);
+
+		List<Long> test1 = new LinkedList<>();
+		test1.add(1L);
+		test1.add(null);
+		test1.add(2L);
+		test1.add(1L);
+		test1.add(null);
+		test1.add(2L);
+		test1.add(1L);
+
+		assertThat(outputTopic.readValuesToList()).isEqualTo(test1);
+	}
+}
